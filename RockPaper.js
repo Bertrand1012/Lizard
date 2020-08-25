1eb5cf5812e69b8b597182bd567e6693f2cd57f7
Bertrand1012 <69489619+Bertrand1012@users.noreply.github.com>
2020-08-24 20:30:12 -0500

    ready to upload new files to get started

---
+++ RockPaper.js
@@ -0,0 +1,67 @@
+
+"use strict";
+
+//Parent Class
+/*class playerChoices         {
+    constructor(hands) {
+        this.availableChoices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+                        }   
+
+runGame () {*/
+
+
+displayRules();
+
+
+
+
+
+function displayRules() {
+console.log ("Welcome to Rock, Paper Scissors, Lizard and Spock or RPSLS. " )
+console.log ("This is a 2 Player game that will either be player 1 vs player 2 or player 1 vs computer.")
+console.log ("Best of 3 games will win!")
+console.log ("choices of weapons determine winner")
+console.log ("Weapon chosen values are as stated")
+console.log ("Rock crushes Scissors" + "\n");
+console.log           ( "Rock crushes Lizard" +  "\n");
+console.log          ( "Paper covers Rock" +  "\n");
+console.log           ("Paper disproves Spock" +  "\n");
+console.log            ("Scissors cuts Paper" +  "\n");
+console.log          ( "Scissors decapitates Lizard" +  "\n");
+console.log           ("Lizard eats Paper" +  "\n");
+console.log           ("Lizard poisons Spock"  +  "\n");
+console.log           ("Spock smashes Scissors" +  "\n");
+console.log          ("Spock vaporizes Rock");
+}
+
+
+
+
+/*   RULES
+* Rock crushes Scissors  
+Rock crushes Lizard
+Paper covers Rock 
+Paper disproves Spock 
+Scissors cuts Paper 
+Scissors decapitates Lizard
+Lizard eats Paper 
+Lizard poisons Spock 
+Spock smashes Scissors 
+Spock vaporizes Rock*/  
+
+/*classWeapons {
+    constructor(weapons){
+/////////////////Winning choices      inheritance code to re-use?????
+(playerOne.choice == "Rock"             playerTwo.choice == "Scissors")
+(playerOne.choice == "Rock"             playerTwo.choice == "Lizard") 
+(playerOne.choice == "Paper"            playerTwo.choice == "Rock") 
+(playerOne.choice == "Paper"            playerTwo.choice == "Spock") 
+(playerOne.choice == "Scissors"         playerTwo.choice == "Paper")
+(playerOne.choice == "Scissors"         playerTwo.choice == "Lizard") 
+(playerOne.choice == "Lizard"           playerTwo.choice == "Paper")
+(playerOne.choice == "Lizard"           playerTwo.choice == "Spock")
+(playerOne.choice == "Spock"            playerTwo.choice == "Scissors")
+(playerOne.choice == "Spock"            playerTwo.choice == "Rock")*/
+
+
+
